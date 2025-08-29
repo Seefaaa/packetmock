b7b84974aa18e0b62d9d4d622579ee73e197e92f
Sefa <50076109+Seefaaa@users.noreply.github.com>
2025-08-29 14:12:17 +0300

    feat: add app icon

--- build.rs
+++ build.rs
@@ -3,13 +3,15 @@ compile_error!("This crate only supports Windows");
 
 use std::error::Error;
 
-use winres::{self, WindowsResource};
+use winres::WindowsResource;
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("cargo:rerun-if-changed=app.manifest");
+    println!("cargo:rerun-if-changed=resources/icon.ico");
 
     let mut resource = WindowsResource::new();
     resource.set_manifest(include_str!("app.manifest"));
+    resource.set_icon("resources/icon.ico");
     resource.compile()?;
 
     Ok(())
