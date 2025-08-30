6a1a8d7587d646b509cacc1fcf51879d8b65b0e2
Sefa <50076109+Seefaaa@users.noreply.github.com>
2025-08-31 00:55:51 +0300

    chore: remove env resolution of windivert

--- windivert-sys/build.rs
+++ windivert-sys/build.rs
@@ -4,35 +4,24 @@ compile_error!("This crate only supports Windows");
 use std::{env::var, error::Error, path::PathBuf};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let base_path = PathBuf::from("../windivert")
-        .canonicalize()
-        .ok()
-        .or_else(|| var("LIBWINDIVERT_PATH").map(PathBuf::from).ok())
-        .ok_or("WinDivert library not found. Please set LIBWINDIVERT_PATH environment variable")
-        .unwrap();
+    let base_path = PathBuf::from("../windivert").canonicalize()?;
 
-    let arch_dir = if cfg!(target_arch = "x86_64") {
+    let header_file = base_path.join("include/windivert.h");
+    let lib_path = base_path.join(if cfg!(target_arch = "x86_64") {
         "x64"
     } else {
         "x86"
-    };
-
-    let header_file = base_path.join("include/windivert.h");
-    let lib_path = base_path.join(arch_dir);
+    });
 
     if !header_file.exists() {
         panic!("WinDivert header not found at {}", header_file.display());
     }
     if !lib_path.exists() {
-        panic!(
-            "WinDivert library directory not found at {}",
-            lib_path.display()
-        );
+        panic!("WinDivert library not found at {}", lib_path.display());
     }
 
     println!("cargo:rustc-link-search={}", lib_path.display());
     println!("cargo:rustc-link-lib=WinDivert");
-    println!("cargo:rerun-if-env-changed=LIBWINDIVERT_PATH");
 
     let bindings = bindgen::Builder::default()
         .header(header_file.to_string_lossy())
